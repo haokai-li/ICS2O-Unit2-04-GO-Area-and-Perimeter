27670eaa1faa340f62dffb12464b54a024c30fc5
haokaili <>
2021-05-07 21:32:37 +0000

    revise7

--- mathInput.go
+++ mathInput.go
@@ -7,11 +7,13 @@ package main
 import "fmt"
 
 func main() {
+	
 	// This function does addition
 	var length int
 	var width int
 	var area int
 	var perimeter int
+	
 	// input
 	fmt.Println("This program finds the area and perimeter of a rectangle.")
 	fmt.Println()
@@ -20,11 +22,13 @@ func main() {
 	fmt.Print("Enter the width (cm): ")
 	fmt.Scanln(&width)
 	fmt.Println()
+	
 	//process
 	area = length * width
 	perimeter = 2 * (length + width)
+	
 	// output
-	fmt.Println("The area is: ", area ," cm².")
-	fmt.Println("The perimeter is: ", perimeter," cm.")
+	fmt.Println("The area is: ", area, " cm².")
+	fmt.Println("The perimeter is: ", perimeter, " cm.")
 	fmt.Println("\nDone.")
 }
